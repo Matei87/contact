7d01f0b58c6db1a40d22f900892f71bc2273c433
Matei Mircea <mrc_matei@yahoo.com>
2020-07-09 00:19:29 +0300

    Updates

---
+++ precache-manifest.6e1cf2e9a70ad92f5fce0b9bba3969cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee69dc3142cb71282a6da3133f503777",
+    "url": "/contact/index.html"
+  },
+  {
+    "revision": "f92b9e53af1d13efdde8",
+    "url": "/contact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "84235b83733bc45d4b73",
+    "url": "/contact/static/css/main.a48c6d8f.chunk.css"
+  },
+  {
+    "revision": "f92b9e53af1d13efdde8",
+    "url": "/contact/static/js/2.5a0cb94d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact/static/js/2.5a0cb94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84235b83733bc45d4b73",
+    "url": "/contact/static/js/main.a53ca435.chunk.js"
+  },
+  {
+    "revision": "618f1611a88a4260a29b",
+    "url": "/contact/static/js/runtime-main.6f16a27e.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/contact/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "647d5243b897c4309bf304182cf02d3a",
+    "url": "/contact/static/media/notfound.647d5243.png"
+  }
+]);
